20fe13e03e8627436cf934c3203c3efa4139264b
Kenniusboggs <ogbogukenny@yahoo.com>
2024-04-03 02:48:56 +0100

    Server env config almost done

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,35 +1,37 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import axios from "axios";
+// import axios from "axios";
 
 
-let envData = {};
+// let envData = {};
 
-const fetchEnvConfig = async () => {
-  const response = await axios.get("http://localhost:3001/processvar")
-  .then(()=>{
-    envData = response.data;
-    console.log(response.data);
-    console.log(response.success, response.msg);
-  })
-  .catch((error)=>{
-    console.error(`Error found: ${error}`);
-  })
-}
+// const fetchEnvConfig = async () => {
+//   const response = await axios.get("http://localhost:3001/processvar")
+//   .then(()=>{
+//     envData = response.data;
+//     console.log(response.data);
+//     console.log(response.success, response.msg);
+//   })
+//   .catch((error)=>{
+//     console.error(`Error found: ${error}`);
+//   })
+// }
 
-fetchEnvConfig()
+// fetchEnvConfig()
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   // eslint-disable-next-line no-undef
-  apiKey: envData.REACT_APP_FIREBASE_API_KEY,
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "shosan-online-class.firebaseapp.com",
   projectId: "shosan-online-class",
   storageBucket: "shosan-online-class.appspot.com",
   // eslint-disable-next-line no-undef
-  messagingSenderId: envData.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   // eslint-disable-next-line no-undef
-  appId: envData.REACT_APP_FIREBASE_APP_ID
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
